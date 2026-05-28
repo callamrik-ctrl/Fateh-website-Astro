6ca727b8ec2845344a6d4552b746b9521f0cae6a
callamrik-ctrl <callamrik-ctrl@users.noreply.github.com>
2026-05-28 19:04:17 -0400

    Remove Astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://fatehplumelec.com",
   build: {
     format: "file"
-  },
-  integrations: [sitemap()]
+  }
 });
-
